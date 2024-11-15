529bf3d98da6d725097c16e8fa678f3deb1a49d3
Vishalll08 <yv063424@gmai.com>
2024-11-15 19:48:12 +0530

    commit done

--- calculator/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ label, onClick, className }) => (
-    <button className={`button ${className}`} onClick={() => onClick(label)}>
-        {label}
-    </button>
-);
-
-export default Button;
--- calculator/src/components/ThemeSwitcher.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ThemeSwitcher = ({ onToggleTheme }) => (
-    <div className="theme-switcher">
-        <button onClick={onToggleTheme}>Toggle Theme</button>
-    </div>
-);
-
-export default ThemeSwitcher;
